5dc97976a9c2023640746df1a332843f588b28bf
Younus <cse.younuskhan@gmail.com>
2019-07-17 03:01:34 +0600

    admin login create

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/','HomeController@index');
 Route::get('/backend', 'AdminController@index');
 Route::get('/dashboard', 'AdminController@show_dashboard');
 Route::post('/admin-dashboard', 'AdminController@dashboard');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
